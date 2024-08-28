7ba22c0c27b7cf844925a5de3737f581ce8ddf97
DAgustinAguilera <agustindanielaguilera@gmail.com>
2024-08-28 04:55:10 -0300

    cambios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = 4000
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
